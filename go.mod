83cab63ff3862e2d9cbed01334a6509ae27add45
caojie <caojie@corp.netease.com>
2020-04-15 14:35:48 +0800

    add smart ip

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.12
 require (
 	github.com/astaxie/beego v1.12.1
 	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	github.com/wangtuanjie/ip17mon v0.0.0-20190605120156-66aedc5b6371
 )
--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
-import "github.com/astaxie/beego"
+import (
+	"encoding/json"
+	"github.com/astaxie/beego"
+	"github.com/wangtuanjie/ip17mon"
+	"net"
+)
 
+type SmartIP struct {
+	IP string
+	ip17mon.LocationInfo
+}
 type MainController struct {
 	beego.Controller
 }
 
-func (this *MainController)Get(){
-	this.Ctx.WriteString("hello world")
+func (this *MainController) Get() {
+	addr := this.Ctx.Request.RemoteAddr
+	ip, _, _ := net.SplitHostPort(addr)
+	loc, _ := ip17mon.Find(ip)
+	sip := SmartIP{
+		IP:           ip,
+		LocationInfo: *loc,
+	}
+	data, _ := json.Marshal(sip)
+	this.Ctx.WriteString(string(data))
 }
 
-func main(){
-	beego.Router("/",&MainController{})
+func main() {
+	if err := ip17mon.Init("./smart.db"); err != nil {
+		panic(err)
+	}
+	beego.SetStaticPath("/download", "download")
+	beego.Router("/smartip", &MainController{})
 	beego.Run(":6001")
 }
